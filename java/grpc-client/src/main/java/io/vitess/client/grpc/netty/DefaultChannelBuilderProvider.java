8593420d6e22d099e7119b4489dea1bc2e93be38
Jacob Schlather <jacobschlather@gmail.com>
2019-04-18 09:44:13 -0700

    Enable keep alive with a 20s ping for vitess grpc connections. (#20)

--- java/grpc-client/src/main/java/io/vitess/client/grpc/netty/DefaultChannelBuilderProvider.java
+++ java/grpc-client/src/main/java/io/vitess/client/grpc/netty/DefaultChannelBuilderProvider.java
@@ -7,6 +7,8 @@ import io.netty.util.concurrent.DefaultThreadFactory;
 import io.vitess.client.grpc.RetryingInterceptor;
 import io.vitess.client.grpc.RetryingInterceptorConfig;
 
+import java.util.concurrent.TimeUnit;
+
 public class DefaultChannelBuilderProvider implements NettyChannelBuilderProvider {
   private static final EventLoopGroup ELG = new NioEventLoopGroup(
       6,
@@ -24,6 +26,8 @@ public class DefaultChannelBuilderProvider implements NettyChannelBuilderProvide
     return NettyChannelBuilder.forTarget(target)
         .eventLoopGroup(ELG)
         .maxInboundMessageSize(16777216)
+        .keepAliveTime(20, TimeUnit.SECONDS)
+        .keepAliveWithoutCalls(true)
         .intercept(new RetryingInterceptor(config));
   }
 }
