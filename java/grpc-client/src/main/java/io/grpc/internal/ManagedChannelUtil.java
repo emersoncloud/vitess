ce8c06899dd2b4304b9f865f74eb53ed37951239
Jacob Schlather <jacobschlather@gmail.com>
2019-07-11 12:44:22 -0700

    Fix a different cast. (#31)

--- java/grpc-client/src/main/java/io/grpc/internal/ManagedChannelUtil.java
+++ java/grpc-client/src/main/java/io/grpc/internal/ManagedChannelUtil.java
@@ -2,6 +2,7 @@ package io.grpc.internal;
 
 import io.grpc.CallOptions;
 import io.grpc.Channel;
+import io.grpc.LoadBalancer;
 import io.grpc.Metadata;
 import io.grpc.netty.ClientTransportShade;
 import io.vitess.proto.grpc.VitessGrpc;
@@ -11,24 +12,31 @@ import java.lang.reflect.Field;
 public class ManagedChannelUtil {
     private static Field clientTransportProvider;
     private static Field delegate;
+    private static Field lastPicker;
     static {
         try {
             clientTransportProvider = ManagedChannelImpl.class.getDeclaredField("transportProvider");
             delegate = ForwardingManagedChannel.class.getDeclaredField("delegate");
+            lastPicker = DelayedClientTransport.class.getDeclaredField("lastPicker");
         } catch (NoSuchFieldException ex) {
             throw new RuntimeException("Unable to build janky shading utility", ex);
         }
         delegate.setAccessible(true);
         clientTransportProvider.setAccessible(true);
+        lastPicker.setAccessible(true);
     }
 
     public static String getLocalAddress(Channel channel) {
         try {
             ManagedChannelImpl impl = (ManagedChannelImpl) delegate.get(channel);
             ClientCallImpl.ClientTransportProvider provider = (ClientCallImpl.ClientTransportProvider) clientTransportProvider.get(impl);
-            return ClientTransportShade.getLocalAddress(provider.get(
-                    new PickSubchannelArgsImpl(VitessGrpc.getExecuteMethod(), new Metadata(), CallOptions.DEFAULT))
-            );
+            PickSubchannelArgsImpl pickSubchannelArgs = new PickSubchannelArgsImpl(VitessGrpc.getExecuteMethod(), new Metadata(), CallOptions.DEFAULT);
+            DelayedClientTransport transport = (DelayedClientTransport) provider.get(pickSubchannelArgs);
+            LoadBalancer.SubchannelPicker picker = (LoadBalancer.SubchannelPicker) lastPicker.get(transport);
+            if (picker == null) {
+                return "Unable to resolve local host via reflection.";
+            }
+            return ClientTransportShade.getLocalAddress(GrpcUtil.getTransportFromPickResult(picker.pickSubchannel(pickSubchannelArgs), false));
         } catch (IllegalAccessException ex) {
             throw new RuntimeException(ex);
         }
