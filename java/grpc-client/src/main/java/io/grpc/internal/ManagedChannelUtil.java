35c6a379fcad4dd59d58b38f942a849b12507360
Jacob Schlather <jacobschlather@gmail.com>
2019-07-11 12:15:54 -0700

    Fix class cast, missed a layer of indirection. (#30)

--- java/grpc-client/src/main/java/io/grpc/internal/ManagedChannelUtil.java
+++ java/grpc-client/src/main/java/io/grpc/internal/ManagedChannelUtil.java
@@ -10,18 +10,21 @@ import java.lang.reflect.Field;
 
 public class ManagedChannelUtil {
     private static Field clientTransportProvider;
+    private static Field delegate;
     static {
         try {
             clientTransportProvider = ManagedChannelImpl.class.getDeclaredField("transportProvider");
+            delegate = ForwardingManagedChannel.class.getDeclaredField("delegate");
         } catch (NoSuchFieldException ex) {
             throw new RuntimeException("Unable to build janky shading utility", ex);
         }
+        delegate.setAccessible(true);
         clientTransportProvider.setAccessible(true);
     }
 
     public static String getLocalAddress(Channel channel) {
         try {
-            ManagedChannelImpl impl = (ManagedChannelImpl) channel;
+            ManagedChannelImpl impl = (ManagedChannelImpl) delegate.get(channel);
             ClientCallImpl.ClientTransportProvider provider = (ClientCallImpl.ClientTransportProvider) clientTransportProvider.get(impl);
             return ClientTransportShade.getLocalAddress(provider.get(
                     new PickSubchannelArgsImpl(VitessGrpc.getExecuteMethod(), new Metadata(), CallOptions.DEFAULT))
